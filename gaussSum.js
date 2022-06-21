0768cd58b7d77bbdc65719a649699421b653ec6e
Getabicha <99878009+Getabicha-Dan@users.noreply.github.com>
2022-06-21 11:36:42 +0200

    Update gaussSum.js
    -not clear on the  print line to be corrected .Is  it the " " is forgotten or the space ?

--- gaussSum.js
+++ gaussSum.js
@@ -5,8 +5,10 @@ Prints the sum of 1+2+3+4...+98+99
 Output: "The sum is 4950"*/
 
 let totalSum = 0;
-for(i = 1;i <= 99; i++){
+
+for(i = 1 ; i <= 99 ; i++){
 
     totalSum = totalSum + i;     
-    }
-console.log('The sum is  '+ totalSum);
+}
+
+console.log('The sum is '+ totalSum);
