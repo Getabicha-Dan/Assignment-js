835378f4e08a459bda4dc8f5a74859e68f2916a5
Getabicha <99878009+Getabicha-Dan@users.noreply.github.com>
2022-06-22 15:54:40 +0200

    Update stockcheker4.js

--- stockcheker4.js
+++ stockcheker4.js
@@ -4,23 +4,25 @@ Write a function that takes the products and a product name.
 	the product with the given name. 
 	(The function deletes the given product name if it exists)*/
     
-    let phoneStore=[{name: "Iphone 13", price: 1000, inStock:15},
-    {name: "Iphone 10", price: 900, inStock:0},
-    {name: "Samsung 22", price: 1200, inStock:9},
-    {name: "OnePlus Pro", price: 850, inStock:0},
-    {name: "Nokia Note", price: 950, inStock:8},
-    {name: "Iphone 11", price: 1000, inStock:12}];
+let phoneStore=[{name: "Iphone 13", price: 1000, inStock:15},
+{name: "Iphone 10", price: 900, inStock:0},
+{name: "Samsung 22", price: 1200, inStock:9},
+{name: "OnePlus Pro", price: 850, inStock:0},
+{name: "Nokia Note", price: 950, inStock:8},
+{name: "Iphone 11", price: 1000, inStock:12}];
 
 function stockCheker3(phoneStore,nameProduct){
-let phoneStoreNew = [ ] ;
+	let phoneStoreNew = [] ;
 
-for(i=0;i < phoneStore.length; i++){
+	for(i=0;i < phoneStore.length; i++){
 
-if(nameProduct !== phoneStore[i].name){
+		if(nameProduct !== phoneStore[i].name){
 
-phoneStoreNew.push(phoneStore[i]);             
-} 
-}
-return phoneStoreNew;
+		phoneStoreNew.push(phoneStore[i]);
+			
+		} 
+		
+	}
+	return phoneStoreNew;
 }
 stockCheker3(phoneStore,"Iphone 11");
